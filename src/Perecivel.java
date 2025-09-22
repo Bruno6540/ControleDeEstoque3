c5121ad13309d02686a5f195f2db42e4e7820cc2
Bruno Fernandes <brfernandes.silva@gmail.com>
2025-09-22 12:04:23 -0300

    Update Perecivel.java

--- src/Perecivel.java
+++ src/Perecivel.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Perecivel extends Produto {
+    private String validade;
 
-public class Perecivel {
+    public Perecivel(String nome, double preco, int quantidade, String validade) {
+        super(nome, preco, quantidade);
+        this.validade = validade;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Validade: " + validade;
+    }
 }
